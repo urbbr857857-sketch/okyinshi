33f14c6c9bc12f74c0c72a3b712db129084ecfa6
urbbr857857-sketch <urbbr857857@gmail.com>
2026-08-22 16:31:08 +0800

    Update py_missav.py

--- py_missav.py
+++ py_missav.py
@@ -16,7 +16,7 @@ class Spider(Spider):
         return "MissAV"
 
     def init(self, extend):
-        self.home_url = 'https://missav.ai'
+        self.home_url = 'https://missav.ws'
         self.headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36",
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
